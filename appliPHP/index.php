641c8289650aba43f83860144f1c6081f1aafb75
Molina-Q <131170327+Molina-Q@users.noreply.github.com>
2023-09-27 16:58:24 +0200

    Add files via upload

---
+++ appliPHP/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Ajout produit</title>
+</head>
+
+<body>
+    <main>
+        <h1>Ajouter un produit</h1>
+        <form action="traitement.php" method="post"> <!-- action indique la cible du formulaire, le fichier à atteindre lorsque l'utilisateur soumettra le formulaire -->
+            <p>
+                <label>
+                    Nom du produit :
+                    <input type="text" name="name">
+                </label>
+            </p>
+            <p>
+                <label>
+                    Prix du produit :
+                    <input type="number" step="any" name="price">
+                </label>
+            </p>
+            <p>
+                <label>
+                    Quantité désirée :
+                    <input type="number" name="qtt" value="1">
+                </label>
+            </p>
+            <p>
+                <input type="submit" name="submit" value="Ajouter le produit">
+            </p>
+        </form>
+
+
+    </main>    
+</body>
+</html>
---
+++ appliPHP/recap.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Récapitulatif des produits</title>
+</head>
+<body>
+    <?php 
+    if(!isset($_SESSION['products'])    ||  empty($_SESSION['products'])){
+
+    }
+    
+    ?>
+</body>
+</html>
+
+<!-- page 17 -->
